f07430636acd186bd2cc6c13b4335893d8337b92
codejoygirl <joyegbu@gmail.com>
2023-09-07 09:35:36 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return: 1 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{	
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
