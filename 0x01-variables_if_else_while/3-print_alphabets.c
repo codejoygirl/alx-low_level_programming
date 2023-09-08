589227b24765603c2305935a49efcaa8e964cae0
codejoygirl <joyegbu@gmail.com>
2023-09-08 14:27:40 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet 'a' to 'z'then 'A' to 'Z'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		putchar (c++);
+	c = 'A';
+	while (c <= 'z')
+		putchar (c++);
+	putchar('\n');
+	return (0);
+}
