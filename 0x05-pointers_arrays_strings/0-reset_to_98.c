3e1760700eac93d7f84029fccef43093c23b40a2
codejoygirl <joyegbu@gmail.com>
2023-09-18 03:23:36 -0700

    read me

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,14 +3,9 @@
 /**
  * reset_to_98 - Updates the value pointed to by a pointer to 98.
  * @n: A pointer to an integer.
+ * Return: Always 0 (Success)
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
