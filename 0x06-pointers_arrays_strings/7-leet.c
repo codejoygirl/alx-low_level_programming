ca91d2b3ceaa3316186743f1ebeff86cacc99028
codejoygirl <joyegbu@gmail.com>
2023-09-21 11:57:28 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,45 +1,26 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * leet - Encodes a string into "leet" speak.
- *
  * @str: The input string to be encoded.
- *
  * Return: A pointer to the modified input string.
  */
 
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-char *leet(char *str)
-{
-	char leet_map[256] = {0};  /* Initialize the mapping array with zeros*/
-    
-    /* Set up the leet mapping*/
-	leet_map['a'] = leet_map['A'] = '4';
-	leet_map['e'] = leet_map['E'] = '3';
-	leet_map['o'] = leet_map['O'] = '0';
-	leet_map['t'] = leet_map['T'] = '7';
-	leet_map['l'] = leet_map['L'] = '1';
-
-char *p = str;
-char *q;
-	int shouldExit = 0;
-	while (*p != '\0' && !shouldExit)
-{
-	if (leet_map[(unsigned char)*p])
-{
-	q = p;
-	while (*q != '\0')
-{
-	if (leet_map[(unsigned char)*q])
-{
-	*q = leet_map[(unsigned char)*q];
-}
-	q++;
-}
-	shouldExit = 1;
-}
-	p++;
-}
-
-	return (str);
-}
+	for (i = 0; n[i] != '\0'; i++
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}	
