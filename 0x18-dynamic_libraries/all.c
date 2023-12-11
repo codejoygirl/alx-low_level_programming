c6447617169f1ce650a086b0008f04a486e5c000
codejoygirl <joyegbu@gmail.com>
2023-12-11 14:58:56 -0800

    ok

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+/**
+ * add - will add two integers
+ * @a: the first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - will subtract two integers
+ * @a: the first int
+ * @b: second int
+ * Return: substract the final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - will divide two integers
+ * @a: the first int
+ * @b: second int
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mul - will multiply two integers
+ * @a: the first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - mod of  two integers
+ * @a: the first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a + b);
+}
--- 0x18-dynamic_libraries/div.c
+++ 0x18-dynamic_libraries/div.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * div - will divide two integers
